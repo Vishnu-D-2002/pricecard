b5bca019adb732bbaf491b0b49dde1703e92806f
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-10-14 11:13:46 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
-function App({ PriceCard1 , PriceCard2 , PriceCard3 }) {
+function App({ PriceCard1, PriceCard2, PriceCard3 }) {
   return (
-    <div id='container'>
-    <div id='first'>
-      <h6>FREE</h6>
-      <h1>$0/month</h1>
-      <ul>
-        {
-          PriceCard1.map(detail => 
-            <li key={detail.id}>{ detail.important ? "✓ " :"× " }{ detail.content }</li>
-          )
-        }
-        </ul>
-        <button disabled>BUTTON</button>
-    </div>
-    <div id='second'>
-      <h6>PLUS</h6>
-      <h1>$9/month</h1>
-      <ul>
-        {
-          PriceCard2.map(detail => 
-            <li key={detail.id}>{ detail.important ? "✓ " :"× " }{ detail.content }</li>
-          )
-        }
+    <div id="container">
+      <div id="first">
+        <h6>FREE</h6>
+        <h1>$0/month</h1>
+        <ul>
+          {PriceCard1.map((detail) => (
+            <li key={detail.id}>
+              {detail.important ? <p>✓ {detail.content}</p> : <p style={{ color: 'lightgrey' }}>× {detail.content}</p>}
+            </li>
+          ))}
         </ul>
         <button disabled>BUTTON</button>
       </div>
-      <div id='third'>
-      <h6>PRO</h6>
-      <h1>$49/month</h1>
-      <ul>
-        {
-          PriceCard3.map(detail => 
-            <li key={detail.id}>{ detail.important ? "✓ " :"× " }{ detail.content }</li>
-          )
-        }
+      <div id="second">
+        <h6>PLUS</h6>
+        <h1>$9/month</h1>
+        <ul>
+          {PriceCard2.map((detail) => (
+            <li key={detail.id}>
+              {detail.important ? <p>✓ {detail.content}</p> : <p style={{ color: 'lightgrey' }}>× {detail.content}</p>}
+            </li>
+          ))}
         </ul>
-        <button type='submit'><a href='#'>BUTTON</a></button>
+        <button disabled>BUTTON</button>
+      </div>
+      <div id="third">
+        <h6>PRO</h6>
+        <h1>$49/month</h1>
+        <ul>
+          {PriceCard3.map((detail) => (
+            <li key={detail.id}>
+              {detail.important ? <p>✓ {detail.content}</p> : <p style={{ color: 'lightgrey' }}>× {detail.content}</p>}
+            </li>
+          ))}
+        </ul>
+        <button type="submit">BUTTON</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
